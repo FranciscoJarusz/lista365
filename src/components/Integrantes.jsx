f3b9626300074f59bd36fda6787983ce28374cda
Francisco Jarusz <franjarusz10@gmail.com>
2025-09-19 11:47:38 -0300

    Cambio foto en cards

--- src/components/Integrantes.jsx
+++ src/components/Integrantes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import directivaImg from '../assets/hero.jpg';
+import cameraIcon from '../assets/camera.svg';
 import linkedinIcon from '../assets/linkedin.svg';
 
 const Integrantes = () => {
@@ -14,7 +14,7 @@ const Integrantes = () => {
             <div className="p-6 flex items-center justify-center gap-12">
                 
                 <div className="flex flex-col items-center justify-center gap-4 bg-gray-50 p-6 rounded-xl w-80 shadow-lg">
-                    <img src={directivaImg} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
+                    <img src={cameraIcon} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
                     <h3 className="text-2xl font-medium text-black">Jesús Ressia</h3>
                     <p className="text-lg text-primario font-bold max-w-2xl">Presidente</p>
                      <a href="/" target="_blank" className="flex items-center gap-4 bg-primario rounded-full px-6 py-2 text-lg text-white hover:scale-105 transition-all duration-300 ease-in-out w-40">
@@ -24,7 +24,7 @@ const Integrantes = () => {
                 </div>
 
                 <div className="flex flex-col items-center justify-center gap-4 bg-gray-50 p-6 rounded-xl w-80 shadow-lg">
-                    <img src={directivaImg} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
+                    <img src={cameraIcon} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
                     <h3 className="text-2xl font-medium text-black">Mateo Schenone</h3>
                     <p className="text-lg text-primario font-bold max-w-2xl">Vice Presidente</p>
                      <a href="/" target="_blank" className="flex items-center gap-4 bg-primario rounded-full px-6 py-2 text-lg text-white hover:scale-105 transition-all duration-300 ease-in-out w-40">
@@ -34,7 +34,7 @@ const Integrantes = () => {
                 </div>
 
                 <div className="flex flex-col items-center justify-center gap-4 bg-gray-50 p-6 rounded-xl w-80 shadow-lg">
-                    <img src={directivaImg} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
+                    <img src={cameraIcon} alt="Presidente" className="w-50 h-50 rounded-full object-cover" />
                     <h3 className="text-2xl font-medium text-black">Fátima Cirulli</h3>
                     <p className="text-lg text-primario font-bold max-w-2xl">Secretaria general</p>
                      <a href="/" target="_blank" className="flex items-center gap-4 bg-primario rounded-full px-6 py-2 text-lg text-white hover:scale-105 transition-all duration-300 ease-in-out w-40">
